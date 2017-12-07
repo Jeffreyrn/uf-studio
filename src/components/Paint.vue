3c1c9c4bb4998efd4524f4e7978d6585bfbefc13
developer <jeffrey@ufactory.cc>
2017-12-07 18:05:18 +0800

    update paint app

--- src/components/Paint.vue
+++ src/components/Paint.vue
@@ -1,42 +1,46 @@
 <template>
   <div class="main-wrapper" id="paint-wrapper">
     <canvas id="fabric" tabindex='1' width="800" height="400"></canvas>
-    <el-button>
+    <el-button @click="undoEvent()">
       <img src="../assets/img/tool-undo.svg" alt="undo">
     </el-button>
-    <el-button>
+    <el-button @click="undoEvent('redo')">
       <img src="../assets/img/tool-redo.svg" alt="redo">
     </el-button>
-    <el-button @click="addTextClick()">
+    <el-button @click="visible.text = true;">
       <img src="../assets/img/tool-text.svg" alt="text">
     </el-button>
     <el-button>
       <img src="../assets/img/tool-image.svg" alt="image">
     </el-button>
-    <el-button>
+    <el-button @click="duplicate()">
       <img src="../assets/img/tool-copy.svg" alt="copy">
     </el-button>
-    <el-button>
+    <el-button @click="removeSelected()">
       <img src="../assets/img/tool-delete.svg" alt="delete">
     </el-button>
+    <el-button @click="removeAll()" :disabled="state.empty">
+      <img src="../assets/img/tool-clear.svg" alt="clear">
+    </el-button>
     <el-dialog
-      title="addText"
+      :title="$t('paintApp.dailog.addText.title')"
       :visible.sync="visible.text"
-      width="30%">
+      width="30%"
+      @close="dialog.textInput=''">
       <span>
-        <el-input type="textarea" :rows="2" placeholder="Please input" v-model="dialog.text"></el-input>
-        <el-select v-model="dialog.select" placeholder="Select">
+        <el-input type="textarea" :rows="2" placeholder="Please input" v-model="dialog.textInput"></el-input>
+        <el-select v-model="dialog.fontSelect" placeholder="Select">
           <el-option
-            v-for="item in FONT_LIST"
-            :key="item.name"
+            v-for="(item, index) in FONT_LIST"
+            :key="index"
             :label="item.name"
-            :value="item.src">
+            :value="index">
           </el-option>
         </el-select>
       </span>
       <span slot="footer" class="dialog-footer">
-        <el-button @click="visible.text = false">Cancel</el-button>
-        <el-button type="primary" @click="visible.text = false">Confirm</el-button>
+        <!-- <el-button @click="visible.text = false">Cancel</el-button> -->
+        <el-button type="primary" @click="addTextAsPath(dialog.textInput)">Confirm</el-button>
       </span>
     </el-dialog>
   </div>
@@ -44,36 +48,49 @@
 <script>
 // import robot from '../assets/lib/robot';
 import { fabric } from 'fabric-webpack';
-// import opentype from 'opentype.js';
+import opentype from 'opentype.js';
 // import Potrace from 'potrace';
-
+const CONFIG = {
+  addPosition: {
+    left: 350,
+    top: 70,
+    width: 100,
+    fontSize: 20,
+  },
+};
 export default {
   data() {
     return {
       playground: null,
+      state: {
+        buffer: [],
+        saved: true,
+        empty: true,
+        backStep: 0,
+      },
       visible: {
         text: false,
         setting: false,
       },
       dialog: {
-        text: '', // text value
-        select: '2', // select font
+        textInput: '', // text value
+        fontSelect: 0, // select font
       },
       FONT_LIST: [
         {
-          name: 'blackLight',
+          name: this.$t('paintApp.fontNameList.blacklight'),
           src: require('../assets/fonts/blackLight.ttf'),
         },
         {
-          name: 'STXingkai',
+          name: this.$t('paintApp.fontNameList.xingkai'),
           src: require('../assets/fonts/STXingkai-SC-Bold.ttf'),
         },
         {
-          name: 'lanting',
+          name: this.$t('paintApp.fontNameList.lanting'),
           src: require('../assets/fonts/lanting.ttf'),
         },
         {
-          name: 'kaiti',
+          name: this.$t('paintApp.fontNameList.kaiti'),
           src: require('../assets/fonts/kanti.ttf'),
         },
       ],
@@ -83,13 +100,117 @@ export default {
     this.playground = new fabric.Canvas('fabric', {
       fireRightClick: true,
     });
+    this.updateModifications();
   },
   methods: {
-    addTextClick() {
-      this.visible.text = true;
+    addTextAsPath(text) {
+      if (text.trim()) {
+        opentype.load(this.FONT_LIST[this.dialog.fontSelect].src, (err, font) => {
+          if (err) {
+            this.$message(`Could not load font: ${err}`);
+          }
+          else {
+            const textpath = font.getPath(text, 0, 150, 72);
+            // console.log(textpath)
+            const path = new fabric.Path(textpath.toPathData(2));
+            // const scale = 100 / path.width;
+            path.set({
+              left: CONFIG.addPosition.left,
+              top: CONFIG.addPosition.top,
+              fontSize: CONFIG.addPosition.fontSize,
+              // scaleX: scale,
+              // scaleY: scale,
+              fill: 'black',
+              stroke: 'black',
+              strokeWidth: 1,
+            });
+            this.playground.add(path);
+            this.updateModifications();
+            this.visible.text = false; // close dialog
+          }
+          // console.log(font);
+        });
+      }
+      else {
+        this.$message(this.$t('recordApp.dialog.name.emptyAlert'));
+      }
+    },
+    updateModifications() {
+      this.state.buffer.push(JSON.stringify(this.playground));
+      this.state.saved = false;
+      // fabric.log(myjson);
+      this.state.empty = this.playground.isEmpty();
+      console.log(this.state.buffer.length, this.state.backStep);
+    },
+    undoEvent(reverse = false) { // do redo when reverse is true
+      const canvas = this.playground;
+      const size = this.state.buffer.length;
+      let checkValid;
+      let nextStep;
+      if (reverse) { // redo
+        checkValid = this.state.backStep > 0;
+        nextStep = this.state.backStep - 1;
+      }
+      else { // undo
+        checkValid = this.state.backStep < (size - 1);
+        nextStep = this.state.backStep + 1;
+      }
+      if (checkValid) {
+        canvas.clear().renderAll();
+        this.state.backStep = nextStep;
+        const loadJsonStr = this.state.buffer[size - nextStep - 1];
+        canvas.loadFromJSON(loadJsonStr, canvas.renderAll.bind(canvas));
+      }
+    },
+    duplicate() {
+      const activeObject = this.playground.getActiveObject();
+      if (activeObject === null) {
+        const activeGroup = this.playground.getActiveGroup();
+        const objects = activeGroup.getObjects();
+        this.playground.discardActiveGroup();
+        objects.forEach((obj) => {
+          const objTop = obj.originalTop + 5;
+          const objLeft = obj.originalLeft + 5;
+          obj.clone((o) => {
+            o.set('left', objLeft);
+            o.set('top', objTop);
+            this.playground.add(o);
+            this.playground.discardActiveGroup();
+          });
+        });
+        this.playground.discardActiveGroup().renderAll();
+      }
+      else {
+        activeObject.clone((obj) => {
+          obj.set('left', obj.left + 8);
+          obj.set('top', obj.top + 8);
+          this.playground.add(obj);
+        });
+      }
+      this.updateModifications();
+    },
+    removeSelected() {
+      let deleteObj = this.playground.getActiveObject();
+      if (!deleteObj) {
+        deleteObj = this.playground.getActiveGroup();
+      }
+      this.playground.remove(deleteObj);
+      this.updateModifications();
+    },
+    removeAll() {
+      this.$confirm(this.$t('paintApp.dailog.deleteall.msg'), {
+        confirmButtonText: this.$t('paintApp.dailog.okBtn'),
+        cancelButtonText: this.$t('paintApp.dailog.cancelBtn'),
+        type: 'warning',
+        dangerouslyUseHTMLString: true,
+      }).then(() => {
+        this.playground.clear().renderAll();
+        this.updateModifications();
+      });
     },
   },
   beforeDestroy() {
+    this.playground = null;
   },
   watch: {
   },
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue';
 import CodeMirror from 'vue-codemirror-lite';
 import ElementUI from 'element-ui';
+import VueI18n from 'vue-i18n';
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App';
 import router from './router';
@@ -11,11 +12,21 @@ import './assets/css/global.css';
 
 Vue.use(CodeMirror);
 Vue.use(ElementUI);
+Vue.use(VueI18n);
 robot.connect();
 Vue.config.productionTip = false;
 
+const i18n = new VueI18n({
+  locale: 'en',
+  silentTranslationWarn: true,
+  messages: {
+    en: require('./assets/i18n/en'),
+    cn: require('./assets/i18n/cn'),
+  },
+});
 /* eslint-disable no-new */
 new Vue({
+  i18n,
   el: '#app',
   router,
   template: '<App/>',
