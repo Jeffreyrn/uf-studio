c6e7c38b1fcac351dd0d9a6a5f97a41b95a35ea5
chao <chao@AlexdeMacBook-Pro-2.local>
2017-12-21 20:46:36 +0800

    separate

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -6,12 +6,7 @@
     <div style="float:left;background-color:yellow;width:100%;">
       <ProjMenu style="float:left;width:15%;"></ProjMenu>
       <div style="float:left;width:85%;background-color:green;">
-        <codemirror
-          v-model="code"
-          :options="editorOption"
-          ref="myEditor"
-          @input="onEditorCodeChange">
-        </codemirror>
+        <CodeEditor></CodeEditor>
         <div style="height:20px;background-color:green;">
           Run
         </div>
@@ -22,110 +17,29 @@
   </div>
 </template>
 <script>
-// import robot from '../assets/lib/robot';
-import { codemirror, CodeMirror } from 'vue-codemirror';
-import 'codemirror/lib/codemirror.css';
-import 'codemirror/theme/monokai.css';
-import 'codemirror/addon/hint/show-hint';
-import 'codemirror/addon/hint/show-hint.css';
-import 'codemirror/addon/hint/javascript-hint';
-import 'codemirror/mode/python/python';
-import 'codemirror/addon/selection/active-line';
-import 'codemirror/addon/edit/matchbrackets';
-import 'codemirror/addon/edit/closebrackets';
-import 'codemirror/addon/fold/foldgutter';
-import 'codemirror/addon/fold/foldgutter.css';
-import 'codemirror/addon/display/fullscreen';
-import 'codemirror/addon/display/fullscreen.css';
-import PythonHint from '../assets/lib/python-hint';
-
 import ProjMenu from './editor/ProjMenu';
-
-// require('codemirror/mode/clike/clike.js');
-// require('codemirror/addon/comment/comment.js');
-// require('codemirror/addon/dialog/dialog.js');
-// require('codemirror/addon/dialog/dialog.css');
-// require('codemirror/addon/search/searchcursor.js');
-// require('codemirror/addon/search/search.js');
-// require('codemirror/keymap/emacs.js');
-// require('codemirror/theme/monokai.css');
-// require('codemirror/addon/hint/javascript-hint.js');
+import CodeEditor from './editor/CodeEditor';
 
 import '../assets/css/reseet.css';
-
-// let count = 0;
-// const tmp = setInterval(() => {
-//   if (count > 5) {
-//     clearInterval(tmp);
-//   }
-//   else {
-//     CodeMirror.getLiveHint.push(String(count));
-//     count += 1;
-//   }
-// }, 1000);
-// CodeMirror.getLiveHint = ['11', '22', '33'];
 
 export default {
   data() {
     return {
-      code: 'def as #123',
-      editorOption: {
-        mode: {
-          name: 'python',
-          version: 3,
-          singleLineStringErrors: false,
-        },
-        tabSize: 2,
-        theme: 'monokai',
-        extraKeys: {
-          Tab: (cm) => {
-            const cursorLine = cm.getLine(cm.getCursor().line);
-            console.log(cursorLine, CodeMirror.hint.anyword);
-            cm.showHint({ hint: CodeMirror.hint.python });
-            // CodeMirror.showHint(cm, CodeMirror.hint.coffeescript);
-          },
-          F10: (cm) => {
-            cm.setOption('fullScreen', !cm.getOption('fullScreen'));
-          },
-        },
-        styleSelectedText: true,
-        styleActiveLine: true,
-        highlightSelectionMatches: { showToken: /\w/, annotateScrollbar: true },
-        lineNumbers: true,
-        line: true,
-        lineWrapping: true,
-        matchBrackets: true,
-        autoCloseBrackets: true,
-        foldGutter: true,
-        gutters: ['CodeMirror-linenumbers', 'CodeMirror-foldgutter'],
-      },
     };
   },
   mounted() {
-    CodeMirror.registerHelper('hintWords', 'python', PythonHint);
-    // this.editor.focus();
-    console.log('this is current editor object', this.editor);
-    this.editor.foldCode(CodeMirror.Pos(13, 0));
   },
   methods: {
-    onEditorCodeChange(newCode) {
-      console.log('this is new code', newCode);
-      this.code = newCode;
-    },
   },
   beforeDestroy() {
   },
   watch: {
   },
   computed: {
-    editor() {
-      // get current editor object
-      return this.$refs.myEditor.codemirror;
-    },
   },
   components: {
-    codemirror,
     ProjMenu,
+    CodeEditor,
   },
 };
 </script>
---
+++ src/components/editor/CodeEditor.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    <codemirror
+      v-model="code"
+      :options="editorOption"
+      ref="myEditor"
+      @input="onEditorCodeChange">
+    </codemirror>
+  </div>
+</template>
+
+<script>
+
+import { codemirror, CodeMirror } from 'vue-codemirror';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/monokai.css';
+import 'codemirror/addon/hint/show-hint';
+import 'codemirror/addon/hint/show-hint.css';
+import 'codemirror/addon/hint/javascript-hint';
+import 'codemirror/mode/python/python';
+import 'codemirror/addon/selection/active-line';
+import 'codemirror/addon/edit/matchbrackets';
+import 'codemirror/addon/edit/closebrackets';
+import 'codemirror/addon/fold/foldgutter';
+import 'codemirror/addon/fold/foldgutter.css';
+import 'codemirror/addon/display/fullscreen';
+import 'codemirror/addon/display/fullscreen.css';
+import PythonHint from '../../assets/lib/python-hint';
+
+export default {
+  data() {
+    return {
+      code: 'def as #123',
+      editorOption: {
+        mode: {
+          name: 'python',
+          version: 3,
+          singleLineStringErrors: false,
+        },
+        tabSize: 2,
+        theme: 'monokai',
+        extraKeys: {
+          Tab: (cm) => {
+            const cursorLine = cm.getLine(cm.getCursor().line);
+            console.log(cursorLine, CodeMirror.hint.anyword);
+            cm.showHint({ hint: CodeMirror.hint.python });
+            // CodeMirror.showHint(cm, CodeMirror.hint.coffeescript);
+          },
+          F10: (cm) => {
+            cm.setOption('fullScreen', !cm.getOption('fullScreen'));
+          },
+        },
+        styleSelectedText: true,
+        styleActiveLine: true,
+        highlightSelectionMatches: { showToken: /\w/, annotateScrollbar: true },
+        lineNumbers: true,
+        line: true,
+        lineWrapping: true,
+        matchBrackets: true,
+        autoCloseBrackets: true,
+        foldGutter: true,
+        gutters: ['CodeMirror-linenumbers', 'CodeMirror-foldgutter'],
+      },
+    };
+  },
+  mounted() {
+    CodeMirror.registerHelper('hintWords', 'python', PythonHint);
+    // this.editor.focus();
+    console.log('this is current editor object', this.editor);
+    this.editor.foldCode(CodeMirror.Pos(13, 0));
+  },
+  methods: {
+    onEditorCodeChange(newCode) {
+      console.log('this is new code', newCode);
+      this.code = newCode;
+    },
+  },
+  beforeDestroy() {
+  },
+  watch: {
+  },
+  computed: {
+    editor() {
+      // get current editor object
+      return this.$refs.myEditor.codemirror;
+    },
+  },
+  components: {
+    codemirror,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
