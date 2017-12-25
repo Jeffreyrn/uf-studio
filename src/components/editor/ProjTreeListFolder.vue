9b8baddca625f46288bd4f2a6015866da3e28d2e
chao <chao@AlexdeMacBook-Pro-2.local>
2017-12-25 10:47:06 +0800

    menuid

--- src/components/editor/ProjTreeListFolder.vue
+++ src/components/editor/ProjTreeListFolder.vue
@@ -2,7 +2,7 @@
   <div>
     <template v-for='(data,index) in filesList(superid)'>
       <div v-if="data.type === model.localProjTree.PROJ_TREE_TYPE.FOLDER">
-        <el-submenu :index="data.uuid" name="submenu">
+        <el-submenu :index="data.uuid" name="submenu" :id="genMenuId(data.uuid)">
           <template slot="title">
             <!-- <div style="width:100%;height:100%;background-color:pink;"> -->
               <span name="file-name" :id="genFileId(data.uuid)"> {{ data.name }} </span>
