6c649b8770798ef3c94b228ff7dd7c648c034f53
Jeffrey <jeffrey@ufactory.cc>
2018-01-25 09:31:21 +0800

    add diff, test bearychat bot

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,9 +1,11 @@
 export const joints = (state) => {
   const arr = state.robot.info.axis;
   console.log('robot', state.robot);
+  const ANGLE_DIFF = [45.77, 0, 9.15, 85.42, 1.53, -91.53];
   if (arr && (arr.length > 0)) {
     const values = arr.map(str => Number(str));
     console.log('arr posi print:', values.length);
+    console.log('ANGLE_DIFF:', ANGLE_DIFF);
     console.table(values);
     this.test = values[1];
     return values;
