12f6235c3aba7458b037724c3d8e9e79a73c0f40
zhongsaihong <727887236@qq.com>
2018-03-23 15:39:18 +0800

    blockly my app style

--- src/components/Blockly/FileList.vue
+++ src/components/Blockly/FileList.vue
@@ -1,12 +1,21 @@
 
 <template>
-  <div id="app-list" class="applist">
-    <div v-for="item in model.children" :key="item.uuid" class="block">
-      <div class="file-icon"><span v-text="item.label.slice(0,1)"></span></div>
-      <div class="file-name">{{item.label}}</div>
+  <div style="background: #F5F5F5;">
+    <div class="my-app">
+      MY APP
+    </div>
+    <div id="app-list" class="app-list">
+      <div v-for="(item, index) in model.children" :key="item.uuid" class="block">
+        <div class="file-icon" style="background:#9CC5F7;" v-if="index % 5 === 0"><span v-text="item.label.slice(0,1)"></span></div>
+        <div class="file-icon" style="background:#F8C6DE;" v-if="index % 5 === 1"><span v-text="item.label.slice(0,1)"></span></div>
+        <div class="file-icon" style="background:#B8E986;" v-if="index % 5 === 2"><span v-text="item.label.slice(0,1)"></span></div>
+        <div class="file-icon" style="background:#D5D5D5;" v-if="index % 5 === 3"><span v-text="item.label.slice(0,1)"></span></div>
+        <div class="file-icon" style="background:#9EE0D2;" v-if="index % 5 === 4"><span v-text="item.label.slice(0,1)"></span></div>
+        <div class="file-name">{{item.label}}</div>
+
+      </div>
     </div>
   </div>
-
 </template>
 <script>
 // import eventBus from './eventBus'
@@ -29,21 +38,55 @@ export default {
 </script>
 
 <style scoped lang="scss">
-#app-list {
-  background: #F5F5F5;
-  height: 45vh;
+.my-app {
+  font-family: 'Gotham-Medium';
+  font-size: 1.8rem;
+  color: #444;
+  letter-spacing: -1.56px;
+  padding: 1.6rem 2.2rem;
+}
+.app-list {
+  height: 40vh;
+  overflow-y: auto;
+  padding: 10px 1.6rem;
+  display: inline-block;
+  width: 100%;
   .block {
-    width: 12%;
-    padding: 2%;
-    margin: 5%;
-    display: inline-block;
+    display: inline-flex;
+    flex-direction: column;
+    align-items: center;
+    width: 20%;
+    cursor: pointer;
+    margin-bottom: 24px;
     .file-icon {
       color: white;
-      background: green;
+      font-family: 'Gotham-Book';
+      font-size: 1.8rem;
+      width: 5rem;
+      height: 5rem;
+      line-height: 5rem;
       text-align: center;
-      font-family: Gotham-Book;
-      font-size: 24px;
+    }
+    .file-name {
+      padding-top: 6px;
+      text-align: center;
+      width: 100%;
+      font-size: 14px;
     }
   }
+}
+.app-list::-webkit-scrollbar {
+  width: 8px;
+  height: 8px;
+}
+.app-list::-webkit-scrollbar-thumb {
+  border-radius: 5px;
+  -webkit-box-shadow: inset 0 0 5px rgba(0,0,0,0.2);
+  background: #D8D8D8;;
+}
+.app-list::-webkit-scrollbar-track {
+  -webkit-box-shadow: inset 0 0 5px rgba(231,231,231,0.4);
+  border-radius: 0;
+  background: rgba(231,231,231,0.4);
 }
 </style>
