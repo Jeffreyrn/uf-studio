84477e8b8a7daf128b4e69b436c75f7e1ef939b2
chao <chao@Chao.local>
2018-03-29 10:59:58 +0800

    app store icons

--- src/components/appStore/DefaultApp.vue
+++ src/components/appStore/DefaultApp.vue
@@ -4,21 +4,21 @@
       <ul>
         <li class="com-app-icon">
           <router-link :to="{name: 'Emulator', params:{data: {name: null}} }">
-            <div style="width:100%;height:100%;cursor: pointer;">
+            <div class="background-icon-control">
             </div>
           </router-link>
           <AppTitle :data="{omitname: 'Control'}"></AppTitle>
         </li>
         <li class="com-app-icon">
           <router-link :to="{name: 'Paint', params:{data: {name: null}} }">
-            <div style="width:100%;height:100%;cursor: pointer;">
+            <div class="background-icon-painting">
             </div>
           </router-link>
           <AppTitle :data="{omitname: 'Paint'}"></AppTitle>
         </li>
         <li v-for="(data ,index) in model.localAppsMgr.allApps.default.data" class="com-app-icon">
           <router-link :to="{name: 'AppDetail', params:{data: data}}">
-            <div style="width:100%;height:100%;cursor: pointer;">
+            <div class="background-icon-default">
             </div>
           </router-link>
           <AppTitle :data='data'></AppTitle>
@@ -49,15 +49,29 @@ export default {
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-/*.app-deault-icon {*/
-  /*width: 136px;*/
-  /*height: 136px;*/
-  /*border-radius: 4px;*/
-  /*margin: 1.4rem 3.2rem 1.4rem 0;*/
-  /*display: inline-block;*/
-  /*text-align: center;*/
-  /*background: #F3F5F7;*/
-/*}*/
+.background-icon-painting {
+  width: 100%;
+  height: 100%;
+  cursor: pointer;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-image: url('./../../assets/img/appStore/icon_painting.svg');
+}
+.background-icon-control {
+  width: 100%;
+  height: 100%;
+  cursor: pointer;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-image: url('./../../assets/img/appStore/icon_control.svg');
+}
+.background-icon-default {
+  width: 100%;
+  height: 100%;
+  cursor: pointer;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-image: url('./../../assets/img/appStore/icon_default.svg');
+}
 </style>
